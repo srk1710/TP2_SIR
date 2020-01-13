9bfe600351092314bd85c70b764db4ed8b289e7a
pedromcsousa <pedrosousa1@sapo.pt>
2020-01-13 12:09:24 +0000

    Update Perfil

--- tp2/src/views/Perfil.vue
+++ tp2/src/views/Perfil.vue
@@ -1,19 +1,16 @@
 <template>
   <div style="padding: 50px;">
     <!-- CABECALHO -->
-    <v-row style="padding: 10px; width: 100%;">
-      <v-col class="col-3">
+    <v-row style="width: 100%;">
+      <v-col class="col-2">
         <v-img
           v-bind:src="this.foto"
           id="imagemPerfil"
-          aspect-ratio="1"
-          class="grey lighten-2"
-          max-width="140px"
-          style="border-radius: 50%; max-width: 100px;"
-          align="middle"
+          class="grey lighten-2 perfil"
         ></v-img>
       </v-col>
-      <v-col class="col-3 d-flex align-center mb-6">
+      <v-col class="col-1"></v-col>
+      <v-col class="col-3 d-flex align-center mb-6" style="margin-top: 50px;">
         <v-row>
           <h1 class="font-weight-black" style="font-family: Roboto;">240</h1>
         </v-row>
@@ -21,7 +18,7 @@
           <p class="font-weight-thin text-center" style="font-family: Roboto;">Publicações</p>
         </v-row>
       </v-col>
-      <v-col class="col-3 d-flex align-center mb-6">
+      <v-col class="col-3 d-flex align-center mb-6" style="margin-top: 50px;">
         <v-row>
           <h1 class="font-weight-black" style="font-family: Roboto;">1600</h1>
         </v-row>
@@ -29,7 +26,7 @@
           <p class="font-weight-thin text-center" style="font-family: Roboto;">Seguidores</p>
         </v-row>
       </v-col>
-      <v-col class="col-3 d-flex align-center mb-6">
+      <v-col class="col-3 d-flex align-center mb-6" style="margin-top: 50px;">
         <v-row>
           <h1 class="font-weight-black" style="font-family: Roboto;">2240</h1>
         </v-row>
@@ -38,15 +35,48 @@
         </v-row>
       </v-col>
     </v-row>
-    <v-row style="width: 100%;">
+    <v-row style="margin-top: 20px; padding: 0px 20px; width: 100%;">
       <div>
-        <h3 class="font-weight-black" style="font-family: Roboto;">
-          {{username}}
-        </h3>
+        <h3 class="font-weight-black" style="font-family: Roboto;">{{username}}</h3>
       </div>
     </v-row>
-    <v-row style="width: 100%;">
+    <v-row style="padding-left: 20px; width: 100%;">
       <div>Sobre {{nome}}: {{bio}}</div>
+    </v-row>
+    <!-- PUBLICAÇÕES -->
+    <v-row>
+      <v-col class="col-4 pub">
+        <v-img
+          class="publicacoes"
+          src="https://abrilsuperinteressante.files.wordpress.com/2019/06/site_temponatureza.png"
+        ></v-img>
+      </v-col>
+      <v-col class="col-4 pub">
+        <v-img
+          class="publicacoes"
+          src="https://www.revistaplaneta.com.br/wp-content/uploads/sites/3/2018/06/12_pl540_unesco1.jpg"
+        ></v-img>
+      </v-col>
+      <v-col class="col-4 pub">
+        <v-img
+          class="publicacoes"
+          src="https://br.jetss.com/wp-content/uploads/2018/05/010130171208-o-que-significa-natureza.jpg"
+        ></v-img>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="col-4 pub">
+        <v-img
+          class="publicacoes"
+          src="https://static.mundoeducacao.bol.uol.com.br/mundoeducacao/conteudo_legenda/4235a7c906ca07412869075b58ac3466.jpg"
+        ></v-img>
+      </v-col>
+      <v-col class="col-4 pub">
+        <v-img
+          class="publicacoes"
+          src="https://www.a12.com/source/files/originals/natureza-260802.jpg"
+        ></v-img>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -82,3 +112,16 @@ export default {
   }
 };
 </script>
+<style>
+.publicacoes {
+  width: 100%;
+  height: 300px;
+}
+.pub {
+  padding: 20px;
+}
+.perfil {
+  border-radius: 50%;
+  min-width: 100%;
+}
+</style>
