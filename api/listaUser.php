798efce6b7d90cfac56f3ea29a2c91f78755fe07
pedromcsousa <pedrosousa1@sapo.pt>
2020-01-21 15:54:05 +0000

    search + api

---
+++ api/listaUser.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'connect.php';
+
+header("Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+	
+	if(!isset($_POST['string'])){
+		$response['errors'] = true;
+		$response['message'] = "Missing Parameteres";
+		die(json_encode($response));
+	}
+	
+    try {
+        $PDO->beginTransaction();
+        $query = "SELECT * FROM utilizadores WHERE id LIKE :string1 OR nome LIKE :string2";
+        $stmt = $PDO->prepare($query);
+        $stmt->bindValue(':string1', "%".$_POST['string']."%");
+        $stmt->bindValue(':string2', "%".$_POST['string']."%");
+
+        $stmt->execute();
+
+        $result = $stmt->fetchAll();
+		
+		error_reporting(0);
+		ini_set(“display_errors”, 0 );
+		
+        if (count($result)) {
+            $response['errors'] = false;
+            $response['result'] = $result;
+            die(json_encode($response));
+        } else {
+            $response['errors'] = true;
+            $response['message'] = "Sem restulados";
+            die(json_encode($response));
+        }
+    } catch (PDOException $e) {
+        $response['errors'] = true;
+        $response['message'] = $e->getMessage();
+        die(json_encode($response));
+    }
--- tp2/src/views/Search.vue
+++ tp2/src/views/Search.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div style="padding: 50px;">
+    <v-row>
+      <v-col cols="12">
+        <v-text-field v-model="aProcurar" @change="changeProcurar" prepend-icon="mdi-magnify" label="Pesquisa"></v-text-field>
+      </v-col>
+      {{listaUtilizadores}}
+    </v-row>
   </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      aProcurar: null,
+      listaUtilizadores: []
+    };
+  },
+  methods: {
+    changeProcurar(){
+      var user = {
+        nome: this.aProcurar,
+        id: this.listaUtilizadores.length
+      }
+      this.listaUtilizadores.push(user);
+      const este = this;
+      var params = new URLSearchParams();
+      params.append("string", this.aProcurar);
+      axios({
+        method: "POST",
+        //url: "http://192.168.64.2/API/api/listaUser.php",
+        url: "http://localhost/SIR/TP2_SIR/api/listaUser.php",
+        data: params
+      })
+        .then(function(response) {
+          if (!response.data.errors) {
+            console.log(response.data);
+          } else {
+            console.log(response.data.message);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
